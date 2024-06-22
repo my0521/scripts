fcedf89361529e641467f7482640607b85a96374
my <yeal166@163.com>
2024-06-22 14:34:36 +0800

    init blog

---
+++ hexo/hexo-install.sh
@@ -0,0 +1,9 @@
+npm install hexo-cli -g     #1.部署 hexo 客户端工具（-g ， 全局模式）
+hexo init blog              #2.初始化 blog 站点目录（存放站点配置文件以及资源文件）  
+cd blog                     #3.进入 blog 目录
+npm install                 #4.安装依赖
+# hexo server               #5.启动 hexo 服务
+
+npm install hexo-generator-searchdb --save
+
+git clone https://github.com/next-theme/hexo-theme-next themes/next
